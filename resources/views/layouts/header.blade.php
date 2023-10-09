57d7027251bf2865e96d7aab48da1d74d9892dce
aramayo123 <aramayo420@gmail.com>
2023-10-09 17:17:10 -0300

    node modules

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,8 +8,11 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css"  rel="stylesheet" />
 	<link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.28/dist/sweetalert2.min.css" rel="stylesheet">
     <title>Ecommerce</title>
+    <style>
+
+    </style>
 </head>
 <body>
-    <div class="bg-gray-800">
+    <div class="bg-gray-800" style="min-height: 688px;">
         @include('layouts.nav')
         
